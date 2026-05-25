87745828ec29657b9a1a62d57edb9d600e550d3c
Akash <guduriakash9@gmail.com>
2026-05-25 11:44:36 +0530

    fixed vercel routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./index.css";
 
 import { Provider } from "react-redux";
 import store from "./redux/store";
@@ -8,5 +9,5 @@ import store from "./redux/store";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <App />
-  </Provider>
+  </Provider>,
 );
