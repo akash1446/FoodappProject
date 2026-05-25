5ddfd6ad27408459d5034d8d186039f2b1cde516
Akash <guduriakash9@gmail.com>
2026-05-25 17:16:41 +0530

    some errors solve of code

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,5 @@
 import React from "react";
-
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  Link,
-  useNavigate,
-} from "react-router-dom";
-
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 import "./App.css";
@@ -26,176 +18,112 @@ import Contactus from "./Contactus";
 
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
-// ================= APP CONTENT =================
+function App() {
+  // ================= NAVIGATION =================
+  const navigate = useNavigate();
 
-function AppContent() {
-  // ================= REDUX CART =================
-
-  const cartItems = useSelector((state) => state.cart?.items || []);
-
-  // ================= CART TOTAL QUANTITY =================
+  // ================= REDUX =================
+  const cartItems = useSelector((state) => state.cart?.items) || [];
 
   const cartQuantity = cartItems.reduce(
     (total, item) => total + (item.quantity || 0),
     0,
   );
 
-  // ================= GET USER =================
-
+  // ================= USER =================
   const user = JSON.parse(localStorage.getItem("loggedInUser") || "null");
 
-  // ================= NAVIGATE =================
-
-  const navigate = useNavigate();
-
   // ================= LOGOUT =================
-
   const Logout = () => {
     localStorage.removeItem("loggedInUser");
-
     navigate("/");
   };
 
   return (
     <>
       {/* ================= NAVBAR ================= */}
-
       <nav className="navbar">
-        {/* ================= LOGO ================= */}
-
         <h2 className="logo">
           <i className="fa-solid fa-utensils"></i>
           FOODIE<span>ZONE</span>
         </h2>
 
-        {/* ================= NAV LINKS ================= */}
-
         <div className="nav-links">
           <Link to="/">
-            <i className="fa-solid fa-house"></i>
-            Home
+            <i class="fa-solid fa-house"></i>Home
           </Link>
-
           <Link to="/veg">
-            <i className="fa-solid fa-carrot"></i>
-            Veg
+            <i class="fa-solid fa-carrot"></i>Veg
           </Link>
-
           <Link to="/nonveg">
-            <i className="fa-solid fa-drumstick-bite"></i>
-            NonVeg
+            <i class="fa-solid fa-drumstick-bite"></i>NonVeg
           </Link>
-
           <Link to="/milk">
-            <i className="fa-solid fa-blender"></i>
-            Milk
+            <i class="fa-solid fa-blender"></i>Milk
           </Link>
-
           <Link to="/chocolate">
-            <i className="fa-solid fa-cookie-bite"></i>
-            Chocolate
+            <i class="fa-solid fa-cookie"></i>Chocolate
           </Link>
-
           <Link to="/order">
-            <i className="fa-solid fa-list"></i>
-            Orders
+            <i class="fa-solid fa-bacon"></i>Orders
           </Link>
-
           <Link to="/about">
-            <i className="fa-solid fa-circle-info"></i>
-            About
+            <i class="fa-solid fa-circle-info"></i>About
           </Link>
-
           <Link to="/contact">
-            <i className="fa-solid fa-phone"></i>
-            Contact
+            <i class="fa-solid fa-address-book"></i>Contact
           </Link>
-
-          {/* ================= CART ================= */}
 
           <Link to="/cart" className="cart-link">
-            <i className="fa-solid fa-cart-shopping"></i>
-            Cart
+            <i class="fa-solid fa-cart-arrow-down"></i> Cart
             {cartQuantity > 0 && (
               <span className="cart-badge">{cartQuantity}</span>
             )}
           </Link>
 
-          {/* ================= AUTH ================= */}
-
           <div className="auth-buttons">
             {user ? (
               <>
-                <span className="welcome-text">Welcome, {user.name}!</span>
+                <span className="welcome-text">Welcome, {user.name}</span>
 
                 <button className="logout-btn" onClick={Logout}>
-                  <i className="fa-solid fa-right-from-bracket"></i>
                   Logout
                 </button>
               </>
             ) : (
               <>
-                <Link to="/login" className="login-btn">
-                  <i className="fa-solid fa-right-to-bracket"></i>
-                  Login
-                </Link>
-
-                <Link to="/register" className="register-btn">
-                  <i className="fa-solid fa-user-plus"></i>
-                  Register
+                <Link to="/login">
+                  <i class="fa-solid fa-key"></i>Login
                 </Link>
               </>
             )}
+            <Link to="/register">
+              <i class="fa-solid fa-user"></i>Register
+            </Link>
           </div>
         </div>
       </nav>
 
-      {/* ================= MAIN CONTENT ================= */}
-
+      {/* ================= ROUTES ================= */}
       <div className="layout">
         <main className="content">
           <Routes>
-            {/* ================= ROUTES ================= */}
-
             <Route path="/" element={<Home />} />
-
             <Route path="/veg" element={<Veg />} />
-
             <Route path="/nonveg" element={<NonVeg />} />
-
             <Route path="/milk" element={<Milk />} />
-
             <Route path="/chocolate" element={<Chocolate />} />
-
             <Route path="/cart" element={<Cart />} />
-
             <Route path="/login" element={<Login />} />
-
             <Route path="/register" element={<Register />} />
-
             <Route path="/order" element={<Order />} />
-
             <Route path="/about" element={<Aboutus />} />
-
             <Route path="/contact" element={<Contactus />} />
-
-            {/* ================= DEFAULT ROUTE ================= */}
-
             <Route path="*" element={<Home />} />
           </Routes>
         </main>
       </div>
     </>
-  );
-}
-
-// ================= MAIN APP =================
-
-function App() {
-  return (
-    <BrowserRouter>
-      <AppContent />
-    </BrowserRouter>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
 
+import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+
+import App from "./App";
 import store from "./redux/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
 );
